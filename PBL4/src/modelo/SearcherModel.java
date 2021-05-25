f01b3dc62626bd859fa64d32765dce49b053416d
Aritz1019 <55275249+Aritz1019@users.noreply.github.com>
2021-05-25 14:17:23 +0200

    Add files via upload

---
+++ PBL4/src/modelo/SearcherModel.java
@@ -0,0 +1,17 @@
+package modelo;
+
+public class SearcherModel {
+	
+	public SearcherModel() {
+
+	}
+
+	public Locality getLocality(String textString) {
+		return null;
+	}
+
+	public ProcessedData getProcessedData(Locality localidad) {
+		return null;
+	}
+	
+}
