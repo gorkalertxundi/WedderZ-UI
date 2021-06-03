6b8512088a8bd8bd6b1078a46b050152ba817a35
MikelOrobengoa <mikel.orobengoai@alumni.mondragon.edu>
2021-06-03 14:43:49 +0200

    TreeMaps en todos lados

--- src/edu/wedderz/core/dataaccess/repo/serv/ProcessedDataService.java
+++ src/edu/wedderz/core/dataaccess/repo/serv/ProcessedDataService.java
@@ -2,16 +2,16 @@ package edu.wedderz.core.dataaccess.repo.serv;
 
 import java.sql.Date;
 import java.sql.Timestamp;
-import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import edu.wedderz.core.model.Locality;
 import edu.wedderz.core.model.ProcessedData;
 
 public interface ProcessedDataService {
 
-	public Map<Date, Set<ProcessedData>> getProcessedDataLatest(Locality location, int days);
+	public TreeMap<Date, Set<ProcessedData>> getProcessedDataLatest(Locality location, int days);
 	
-	public Set<ProcessedData> getProcessedDataByDate(Locality location, Timestamp date);
+	public TreeMap<Date, Set<ProcessedData>> getProcessedDataByDate(Locality location, Timestamp date);
 	
 }
