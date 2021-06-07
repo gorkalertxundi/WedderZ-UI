92e0c1e24fb6f7cb803be12943b556fcc97d13be
MikelOrobengoa <mikel.orobengoai@alumni.mondragon.edu>
2021-06-07 22:44:24 +0200

    Station list

--- src/edu/wedderz/core/app/view/StationListView.java
+++ src/edu/wedderz/core/app/view/StationListView.java
@@ -6,6 +6,9 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.util.Collection;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -18,64 +21,98 @@ import edu.wedderz.core.model.Station;
 
 public class StationListView extends JPanel {
 	private static final long serialVersionUID = 1L;
-	
-	//private JTextField textField;
+
+	// private JTextField textField;
 	private JScrollPane listPane;
 	StationListController stationListController;
-	
+
 	public StationListView(StationListController stationListController) {
 		this.stationListController = stationListController;
 		setBackground(Color.BLACK);
 
 		listPane = new JScrollPane();
-		
-		listPane.setPreferredSize(new Dimension(500, 500));
-		
-		listPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+		listPane.setPreferredSize(new Dimension(400, 350));
+
+		listPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		/*
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{43, 116, 73, 81, 0};
-		gridBagLayout.rowHeights = new int[]{25, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		setLayout(gridBagLayout);
-		
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.insets = new Insets(10, 0, 10, 5);
-		gbc_textField.gridx = 1;
-		gbc_textField.gridy = 0;
-		add(stationList, gbc_textField);
-		*/
-		
+		 * GridBagLayout gridBagLayout = new GridBagLayout(); gridBagLayout.columnWidths
+		 * = new int[]{43, 116, 73, 81, 0}; gridBagLayout.rowHeights = new int[]{25, 0,
+		 * 0}; gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0,
+		 * Double.MIN_VALUE}; gridBagLayout.rowWeights = new double[]{0.0, 1.0,
+		 * Double.MIN_VALUE}; setLayout(gridBagLayout);
+		 * 
+		 * GridBagConstraints gbc_textField = new GridBagConstraints();
+		 * gbc_textField.fill = GridBagConstraints.HORIZONTAL; gbc_textField.insets =
+		 * new Insets(10, 0, 10, 5); gbc_textField.gridx = 1; gbc_textField.gridy = 0;
+		 * add(stationList, gbc_textField);
+		 */
+
 		this.add(listPane);
-		
+
 	}
-	
+
 	public void setStationList(Collection<Station> stations) {
 		System.out.println((stations.size() / 2) + stations.size() % 2);
-		JPanel panel = new JPanel(new GridLayout((stations.size() / 2) + stations.size() % 2, 2));
-		for(Station station : stations) {
-			JPanel stationPanel = new JPanel();
+		//JPanel panel = new JPanel(new GridLayout((stations.size() / 2) + stations.size() % 2, 2));
+		JPanel wrapper = new JPanel();
+		wrapper.setLayout(new BoxLayout(wrapper, BoxLayout.PAGE_AXIS));
+		for (Station station : stations) {
+			JPanel stationWrapper = new JPanel();
+			stationWrapper.setLayout(new BoxLayout(stationWrapper, BoxLayout.LINE_AXIS));
+			stationWrapper.setBorder(BorderFactory.createCompoundBorder(
+					BorderFactory.createCompoundBorder(
+					BorderFactory.createEmptyBorder(4, 4, 4, 4),
+					BorderFactory.createLineBorder(Color.BLACK)),
+					BorderFactory.createEmptyBorder(16, 8, 16, 8)
+					));
+			stationWrapper.add(Box.createRigidArea(new Dimension(16, 0)));
 			
-			JLabel iconLabel = new JLabel(String.valueOf(station.getStationId()));
-			iconLabel.setIcon(new ImageIcon(new ImageIcon("icons/estacion.png").getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH)));
-			stationPanel.add(iconLabel);
+			JPanel infoPanel = new JPanel();
+			infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.PAGE_AXIS));
+			infoPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 40));
+
+			JLabel idLabel = new JLabel("Station #" + String.valueOf(station.getStationId()));
+			idLabel.setIcon(new ImageIcon(
+					new ImageIcon("icons/estacion.png").getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH)));
+			infoPanel.add(idLabel);
+			infoPanel.add(Box.createRigidArea(new Dimension(0, 5)));
 			
 			Locality locality = station.getLocality();
-			stationPanel.add(new JLabel(locality != null ? locality.getName() : "Locality not defined"));
+			infoPanel.add(new JLabel("Locality: " + (locality != null
+					? locality.getName() + ", " + locality.getCountry().getCountryId().toUpperCase()
+					: "Not defined")));
+			infoPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+
+			String description = station.getDescription();
+			infoPanel.add(new JLabel("Description: " + (description != null ? description : "Not defined")));
+			infoPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+
+			Double latitude = station.getLatitude();
+			infoPanel.add(new JLabel("Latitude: " + ((latitude != null && latitude != 0) ? String.valueOf(latitude) : "Not defined")));
+			infoPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+
+			Double longitude = station.getLongitude();
+			infoPanel.add(new JLabel("Longitude: " + ((longitude != null && longitude != 0) ?  String.valueOf(longitude) : "Not defined")));
+			infoPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+
+			stationWrapper.add(infoPanel);
+			stationWrapper.add(Box.createHorizontalGlue());
 			
 			JButton button = new JButton();
-			button.setText("Ver estación");
+			button.setText("Ver");
 			button.addActionListener(stationListController);
 			button.setActionCommand(String.valueOf(station.getStationId()));
-			stationPanel.add(button);
+			button.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.BLACK), BorderFactory.createEmptyBorder(8, 16, 8, 16)));
+			stationWrapper.add(button);
+			stationWrapper.add(Box.createRigidArea(new Dimension(16, 0)));
 			
-			panel.add(stationPanel);
+			
+
+			wrapper.add(stationWrapper);
 		}
-		listPane.setViewportView(panel);
+		listPane.setViewportView(wrapper);
 		listPane.repaint();
 	}
 
-	
 }
