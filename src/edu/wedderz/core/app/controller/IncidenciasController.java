9d0b2f262a911493a305ab18ae6ac73edce8410a
iarambururo <iarambururo@gmail.com>
2021-06-13 21:22:13 +0200

    cositas

--- src/edu/wedderz/core/app/controller/IncidenciasController.java
+++ src/edu/wedderz/core/app/controller/IncidenciasController.java
@@ -16,88 +16,78 @@ import edu.wedderz.core.app.view.IncidenciasAdmin;
 
 public class IncidenciasController implements ActionListener {
 
-	
-	 
 	IncidenciasAdmin incidencias;
-	
-	
-	public IncidenciasController( IncidenciasAdmin incidencias) {
-		
-		this.incidencias=incidencias;
+
+	String estacionID;
+
+	public IncidenciasController(IncidenciasAdmin incidencias) {
+
+		this.incidencias = incidencias;
 	}
-	
-	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		switch (e.getActionCommand()) {
-			case "buttonEnviar":
-	
-				String asuntoString =incidencias.getEditorPaneAsunto();
-				String descripcionString = incidencias.getEditorPaneDescripcion();
-				JEditorPane asunto = null;
-				asunto.setText("asunto");
-				JEditorPane descripcion = null;
-				descripcion.setText("descripcion");
-				
-						
-				
-				if ((asuntoString.isEmpty() || descripcionString.isEmpty())) {
-					
-					System.out.println("no pusiste nada");
-					JOptionPane.showMessageDialog(null,"Rellena los dos campos ");
-					
-				}else {
-					
-					JOptionPane.showMessageDialog(null,"Tu incidencia se ha mandado correctamente");
-	
-				}
-				System.out.println("entra");
-				
-				
-				
-				break;
-			case "buttonActualizar":
-				if(incidencias.getComboBoxValue().equals("dos")) {
-					
-					mostrarTexto();
-					
-				}else {
-					incidencias.editorPaneAsunto.setText("");
-					incidencias.editorPaneDescripcion.setText("");
-				}
-				
-				break;
-			case "Iniciar sesión":
-				break;
-			case "Mis estaciones":
-				break;
-			case "Mis Incidencias":
-				
-				
-				
-				break;
-			case "Comprar estaciones":
-				
-				break;
-			default:
-				break;
+		case "buttonEnviar":
+
+			String asuntoString = incidencias.getEditorPaneAsunto();
+			String descripcionString = incidencias.getEditorPaneDescripcion();
+			JEditorPane asunto = null;
+			asunto.setText("asunto");
+			JEditorPane descripcion = null;
+			descripcion.setText("descripcion");
+
+			if ((asuntoString.isEmpty() || descripcionString.isEmpty())) {
+
+				System.out.println("no pusiste nada");
+				JOptionPane.showMessageDialog(null, "Rellena los dos campos ");
+
+			} else {
+
+				JOptionPane.showMessageDialog(null, "Tu incidencia se ha mandado correctamente");
+
 			}
+			System.out.println("entra");
+
+			break;
+		case "buttonActualizar":
+			/*
+			 * if(getIncidentComprobar(incidencias.getComboBoxValue())) {
+			 * 
+			 * 
+			 * getIncident(incidencias.getComboBoxValue());
+			 * 
+			 * mostrarTexto();
+			 * 
+			 * }else { incidencias.editorPaneAsunto.setText("");
+			 * incidencias.editorPaneDescripcion.setText(""); }
+			 */
+			break;
+		case "buttonAceptar":
+
+			//setIncidentAcceptDeny(asunto, estacionID, true);
+
+			break;
+		case "buttonDenegar":
+
+		//	setIncidentAcceptDeny(asunto, estacionID, false);
+
+			break;
+	
+		default:
+			break;
 		}
-	
-public void mostrarTexto() {
-	incidencias.editorPaneAsunto.setText("asunto");
-	incidencias.editorPaneDescripcion.setText("Descripcion");
 	}
 
-
+	public void mostrarTexto() {
+		incidencias.editorPaneAsunto.setText("asunto");
+		incidencias.editorPaneDescripcion.setText("Descripcion");
 	}
-	
-	
-	
-	
-	
-	
-	
 
+	public void setIncidentAcceptDeny(JEditorPane asunto, String estacionID, Boolean f) {
 
+		incidencias.editorPaneAsunto.setText("asunto");
+		incidencias.editorPaneDescripcion.setText("Descripcion");
+	}
+
+}
